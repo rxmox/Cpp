3e9d007bcea8029b58b51d672b5f5f3fd42d2d77
Omar <omar52ramos@gmail.com>
2023-06-26 15:29:35 -0600

    small fix

--- MyCode/Assignment1-LetterPyramid/main.cpp
+++ MyCode/Assignment1-LetterPyramid/main.cpp
@@ -8,19 +8,19 @@ int main(){
     cout << "Enter a string: ";
     cin >> input_string;
 
-    int string_length = input_string.length();
-    int space_count = string_length - 1;
+    int string_length = input_string.length();              // length of the string
+    int space_count = string_length - 1;                    // number of spaces to print
 
-    for (int i = 0; i < string_length; i++){
-        for (int j = 0; j < space_count; j++){
+    for (int i = 0; i < string_length; i++){                // loop through the string
+        for (int j = 0; j < space_count; j++){              // loop through the spaces
             cout << " ";
         }
-        space_count--;
+        space_count--;                                      // decrement the number of spaces to print
         // for (int k = 0; k < i; k++){
         //     cout << input_string[k];
         // }
-        cout << input_string.substr(0, i);
-        for (int l = i; l >= 0; l--){
+        cout << input_string.substr(0, i);                 // print the substring of the input string from index 0 to i
+        for (int l = i; l >= 0; l--){                      // loop backwards through the string
             cout << input_string[l];
         }
         cout << endl;
