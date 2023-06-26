6064a5fdbeedef825be6ccd1ff99f3dd33124765
Omar <omar52ramos@gmail.com>
2023-06-26 15:04:15 -0600

    added challenge

---
+++ MyCode/Assignment1-LetterPyramid/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string input_string;
+    cout << "Enter a string: ";
+    cin >> input_string;
+
+    int string_length = input_string.length();
+    int space_count = string_length - 1;
+
+    for (int i = 0; i < string_length; i++){
+        for (int j = 0; j < space_count; j++){
+            cout << " ";
+        }
+        space_count--;
+        for (int k = 0; k < i; k++){
+            cout << input_string[k];
+        }
+        for (int l = i; l >= 0; l--){
+            cout << input_string[l];
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
